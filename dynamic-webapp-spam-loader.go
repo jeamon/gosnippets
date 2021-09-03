908607e4bdd78c3365422393160feccb36c41900
Jerome AMON <cloudmentor.scale@gmail.com>
2021-09-03 21:37:40 +0200

    refactored the full code - more comments

--- dynamic-webapp-spam-loader.go
+++ dynamic-webapp-spam-loader.go
@@ -1,76 +1,97 @@
 package main
 
-import("time";"os";"sync";"log";"bufio";"strings")
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"sync"
+	"time"
+)
 
 var spamWords []string
 var addSpamMutex *sync.RWMutex
 var spamLatestStat os.FileInfo
 
-// intialization
 func init() {
-	
+	// initialize global spamWords list RW mutex.
 	addSpamMutex = &sync.RWMutex{}
-	loadSpamWords() // initial loading of spam words from file
+	// always load spam words from file at startup.
+	loadSpamWords()
 }
 
-// load spam wordslist and save
+// loadSpamWords read the content of the file "config/spam-words.txt" and add each line to the
+// spam words list. It also update the spamLatestStat variable with the file attributes.
 func loadSpamWords() {
 	var spam string
 	fichier, err := os.Open("config/spam-words.txt")
-    if err != nil {
-    	log.Println("[ Eror ] Failed to load spam words file. ErrMsg -", err)
-        os.Exit(1)
-    }
-     
-    defer fichier.Close()
-    
-    spamLatestStat, err = fichier.Stat()
-    if err != nil {
+	if err != nil {
+		log.Println("[ Eror ] Failed to load spam words file. ErrMsg -", err)
+		os.Exit(1)
+	}
+
+	defer fichier.Close()
+
+	spamLatestStat, err = fichier.Stat()
+	if err != nil {
 		log.Println("[ Eror ] Failed to get latest statistics of spam words file. ErrMsg -", err)
 	}
 
-    scanner := bufio.NewScanner(fichier)
-    addSpamMutex.Lock() // On verrou la liste pour manipulation 
-    // as needed to reflect same state as file - clean and recreate the slice
-    spamWords = nil
+	scanner := bufio.NewScanner(fichier)
+	// lock the list for processing.
+	addSpamMutex.Lock()
+	// as needed to reflect same state as file - clean and recreate the slice.
+	spamWords = nil
 	spamWords = []string{}
+	// loop over the file content and build the spam words list.
 	for scanner.Scan() {
-	    spam = strings.Trim(strings.TrimSpace(scanner.Text()),"\r\n")
-	    if spam != "" {
-	    	spamWords = append(spamWords, spam)
-    	}
-    }
-    log.Println(spamWords) // display for checking
-    addSpamMutex.Unlock()
+		spam = strings.Trim(strings.TrimSpace(scanner.Text()), "\r\n")
+		if spam != "" {
+			spamWords = append(spamWords, spam)
+		}
+	}
+	// release the lock.
+	addSpamMutex.Unlock()
+	// display for checking if needed.
+	log.Println(spamWords)
 }
 
-// check every interval hour and update if changes
+// updateSpamWords check every interval hour and update spam words list in case the file changed.
 func updateSpamWords(interval int) {
+
 	for {
 		stat, err := os.Stat("config/spam-words.txt")
 		if err != nil {
 			log.Println("[ Eror ] Failed to get statistics of spam words file. ErrMsg -", err)
 		} else {
 			if stat.Size() != spamLatestStat.Size() || stat.ModTime() != spamLatestStat.ModTime() {
-				loadSpamWords() // load when size or latest modification time changed
+				// size or latest modification time changed so load file content.
+				loadSpamWords()
 			}
 		}
+		// wait until next interval hour(s).
 		time.Sleep(time.Duration(interval) * time.Hour)
 	}
 }
 
-
-// change this file config/spam-words.txt content and observe
+// in case you want to experiment this program change this file
+// "config/spam-words.txt" content and observe the output list.
 func main() {
-	doneChan := make(chan bool)
-	go updateSpamWords(1) // check to update each 1 hour if any changes
-	<- doneChan
+
+	// every 1 hour check for any changes and updates if any.
+	go updateSpamWords(1)
+
+	fmt.Println("\nPress the Enter Key to leave the program.")
+	// wait for Enter Key.
+	fmt.Scanln()
 }
 
+// below is a short demo of how to use this above routine into your contact message handler.
+/*
 
-/* 
-
-type Message struct { 
+// contact submission message format.
+type Message struct {
 	FullName string
 	Email string
 	Subject string
@@ -78,9 +99,9 @@ type Message struct {
 	Errors map[string]string
 }
 
-check if subject or contact is suspicious
+// check if subject or content is suspicious.
 func (msg *Message) isSpamMessage() bool {
-	// acquire the lock and ensure its release
+	// acquire the lock and ensure its release.
 	addSpamMutex.RLock()
 	defer addSpamMutex.RUnlock()
 	// loop over the spam words list and stop once there is a hit
@@ -93,7 +114,7 @@ func (msg *Message) isSpamMessage() bool {
 	return false
 }
 
-// snipped to insert into the contact handler and send fake confirmation for spam message
+// snipped to insert into the contact handler and send fake confirmation for spam message.
 if msg.isSpamMessage() {
 	// routine to handle goes here
 	// you can ignore user message or send fake confirmation
